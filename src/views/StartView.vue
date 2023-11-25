3088662a527f5ce2d615397bb4d3928e1b7e6010
Joshua Marquez <Joshua.marquez0718@gmail.com>
2023-11-25 19:29:28 +0800

    Added tailwindcss

---
+++ src/views/StartView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div className ="flex items-center justify-center h-screen m-0 bg-primary">
+        <div className="h-32 w-32 bg-white text-center"> Logo goes here</div>
+</div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,23 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#D91A2A",
+          "primary-content": "#F2F2F2",
+          "secondary": "#F2B705",
+          "accent": "#e76800",
+          "neutral": "#10222b",
+          "base-100": "#F2F2F2",
+          "info": "#0090af",
+          "success": "#00a200",
+          "warning": "#ffb400",
+          "error": "#e12156",
+        },
+      },
+    ],
+  },
 }
 
