c9be2bae88eeeeab5db28bc581d7c76a0e5fb4ca
Joshua Marquez <Joshua.marquez0718@gmail.com>
2023-11-25 23:58:54 +0800

    added ionic components

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { IonicVue } from '@ionic/vue';
 
-const app = createApp(App)
+const app = createApp(App).use(IonicVue).use(router);
 
 app.use(router)
 
