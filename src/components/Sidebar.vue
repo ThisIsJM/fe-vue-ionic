c9e232fee3896f50f8c111bbb2a93e6a5c9b1272
Joshua Marquez <Joshua.marquez0718@gmail.com>
2023-11-26 19:55:40 +0800

    increased sidebar z index

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -6,7 +6,7 @@
 </script>
 
 <template>
-    <div className="drawer">
+    <div className="drawer z-50">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
             <label htmlFor="my-drawer">
