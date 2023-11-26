d5b3bd52d2ec4cb954ae75f5b55f27850cc9e7c2
Joshua Marquez <Joshua.marquez0718@gmail.com>
2023-11-26 17:00:13 +0800

    modified logo component

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import ReverseLogo from '../assets/reverse-logo.svg';
+import Logo from '../assets/logo.svg';
+
+    const props = withDefaults(defineProps<{
+        width: number
+        height: number
+        reverseColor: boolean
+    }>(), {width:0, height:0, reverseColor:false})
+
+    const getLogo = computed(() => props.reverseColor ? ReverseLogo : Logo);   // const getLogo = '../assets/logo.svg'
+</script>
 <template>
-    <div className="h-32 w-32 bg-white text-center"> Logo goes here</div>
+    <img :src="getLogo" alt="Logo" :width="props.width" :height="props.height">
 </template>
