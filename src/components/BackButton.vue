b43b8ea1122843f9922931e248b0a671d59c04d7
Joshua Marquez <Joshua.marquez0718@gmail.com>
2023-11-27 20:58:10 +0800

    made separate component for back navigate button

---
+++ src/components/BackButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import router from '../router';
+import LeftArrowIcon from './icons/LeftArrowIcon.vue';
+
+    const onNavigateBack = () => {router.go(-1)}
+</script>
+
+<template>
+    <button @click="onNavigateBack" class="btn btn-xs h-8 w-8 btn-gray-400 rounded-xl text-gray-600">
+        <LeftArrowIcon/>
+    </button>
+</template>
